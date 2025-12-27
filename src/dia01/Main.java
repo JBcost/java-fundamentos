cc9f5325c6039789639ed45e0a9ac142472f4eb4
JBcost <143133120+JBcost@users.noreply.github.com>
2025-12-27 14:27:22 -0300

    Add age check for legal age in Main.java

--- src/dia01/Main.java
+++ src/dia01/Main.java
@@ -1,1 +1,17 @@
+package dia01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "João";
+        int idade = 20;
+
+        if (idade >= 18) {
+            System.out.println(nome + " é maior de idade");
+        } else {
+            System.out.println(nome + " é menor de idade");
+        }
+
+    }
+}
 
